e0c49e23318d65f507ae488111168639ca0f3f7c
Matthew Carter <m@ahungry.com>
2019-08-11 16:25:44 -0400

    Allow querying multiple maps

--- server/src/clj/msa/routes/home.clj
+++ server/src/clj/msa/routes/home.clj
@@ -18,23 +18,25 @@
 (defn json-input [i]
   {:body (w/handle-input i)})
 
-(defn json-world-map []
-  {:body (w/get-world-map 0)})
+(defn json-world-map [{:keys [zone]}]
+  (let [zone-n (Integer/parseInt (or zone 0))]
+    {:body (w/get-world-map zone-n)}))
 
-(defn json-world-map-wrapped []
-  {:body {:map (w/get-world-map 0)}})
+(defn json-world-map-wrapped [{:keys [zone]}]
+  (let [zone-n (Integer/parseInt (or zone 0))]
+    {:body {:map (w/get-world-map zone-n)}}))
 
-(defn json-world [i]
-  {:body (w/get-world-step i)})
+(defn json-world [params]
+  {:body (w/get-world-step params)})
 
 (defroutes home-routes
   (OPTIONS "/world-map.json" [] {:body []})
-  (GET "/world-map.json" []
+  (GET "/world-map.json" {params :params}
        (w/logp "World map was hit")
-       (json-world-map))
-  (GET "/world-map-wrapped.json" []
+       (json-world-map params))
+  (GET "/world-map-wrapped.json" {params :params}
        (w/logp "World map wrapped was hit")
-       (json-world-map-wrapped))
+       (json-world-map-wrapped params))
   (OPTIONS "/world.json" [] {:body []})
   (GET "/world.json" {params :params}
        (w/logp "World was hit")
