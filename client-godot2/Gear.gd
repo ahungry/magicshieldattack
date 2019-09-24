bb2792ef92d4b2d0ef2cd9c148f07c3cc4757fcc
Matthew Carter <m@ahungry.com>
2019-09-24 01:09:00 -0400

    Gear page allows switching, neat

--- client-godot2/Gear.gd
+++ client-godot2/Gear.gd
@@ -7,4 +7,17 @@ extends Node2D
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	printt("Gear was loaded")
+	#set_process(true)
+	set_process_input(true)
+
+func _process(delta):
 	pass
+
+func stop_editing_gear():
+	world.goto_scene('res://Main.tscn')
+
+func _input(event):
+	printt("Clicked the booton in Gear.gd")
+	if event.is_action_pressed('ui_gear'):
+		return stop_editing_gear()
