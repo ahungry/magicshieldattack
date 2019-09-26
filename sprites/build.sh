cf6c4222f8b602d533fef437131cfd3fa92106a0
Matthew Carter <m@ahungry.com>
2019-09-25 22:50:04 -0400

    use a build script for better image handling

--- sprites/build.sh
+++ sprites/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Builds the images from small aseprite 32x32 images into 32 * 3 images (maybe we need bigger?)
+
 set +x
 
-for f in $(ls 32-*.png); do
-    #echo $f
+for f in $(ls 32-*.png | grep -vE '[01].png$'); do
     convert $f -crop 32x32 $f
 done
+
+for f in $(ls 32-*.png | grep -E '[01].png$'); do
+    convert $f -filter point -resize 300% $f
+done
+
+ls -thr *png | xargs -I{} identify {}
